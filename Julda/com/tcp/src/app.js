c82526d63466d84295523502b886c5bc79911195
Ubuntu <guswk_pro@naver.com>
2015-12-29 14:39:59 +0000

    msg

--- Julda/com/tcp/src/app.js
+++ Julda/com/tcp/src/app.js
@@ -3,16 +3,23 @@ var http = require('http');
 var path = require('path');
 var dao = require('./dao');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 80);
 
 app.use(express.static(path.join(__dirname, 'view')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('[/, login]', function (req, res) {
-    res.sendfile('index.html');
+app.get('/login', function (req, res) {
+    res.sendfile('./view/index.html');
 });
 
-app.get('/login', dao.login);
+app.post('/login', function(req, res){
+	var req_mem_id = req.body.id;
+	var req_mem_pw = req.body.pw;
+	res.send(req_mem_id + ' ' + req_mem_pw);
+});
 
 
 http.createServer(app).listen(app.get('port'), function () {
