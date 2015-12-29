a3fe659890a6e6182f3f5d1811b1803f8b6e308f
Ubuntu <guswk_pro@naver.com>
2015-12-29 14:02:14 +0000

    aaa

--- Julda/com/tcp/src/app.js
+++ Julda/com/tcp/src/app.js
@@ -4,15 +4,17 @@ var path = require('path');
 var dao = require('./dao');
 var app = express();
 
+app.set('port', process.env.PORT || 80);
+
 app.use(express.static(path.join(__dirname, 'view')));
 
-app.get('/', function (req, res) {
+app.get('[/, login]', function (req, res) {
     res.sendfile('index.html');
 });
 
 app.get('/login', dao.login);
 
 
-http.createServer(app).listen(80, function () {
+http.createServer(app).listen(app.get('port'), function () {
 	console.log('Server running at http://52.192.184.47');
 });
--- Julda/com/tcp/src/dao/index.js
+++ Julda/com/tcp/src/dao/index.js
@@ -10,8 +10,10 @@ var client = mysql.createConnection({
 exports.login = function (request, response) {
 	var req_mem_id = request.param('id');
 	var req_mem_pw = request.param('pw');
+	console.log(req_mem_id);
+	console.log(req_mem_pw);
 
-	client.query('SELECT * FROM mydb.Members where mem_id=? AND mem_pw', [req_mem_id, req_mem_pw], function(error, result, fields) {
+	client.query('SELECT * FROM mydb.Members where mem_id=? AND mem_pw=?', [req_mem_id, req_mem_pw], function(error, result, fields) {
 		if(result.length == 0){
 			response.json({
 				RESULT : "NO"
